bb1aeeb7c4d109edb4aa61a8bf2b4f739e85d280
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-04-08 16:50:46 +0400

    Done app functionality

---
+++ WordpadWPF/Helpers/FileHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordpadWPF.Helpers
+{
+    class FileHelper
+    {
+        public string FileName { get; set; }
+        public string ReadDataFromFile()
+        {
+            var open = new OpenFileDialog();
+            open.Filter = "Txt Files (*.txt)| *.txt";
+
+            if (open.ShowDialog() != true)
+                return String.Empty;
+
+            FileName = open.FileName;
+
+            return File.ReadAllText(FileName);
+        }
+
+        public void SaveDataToFile(string data)
+        {
+            var save = new SaveFileDialog();
+
+            save.Filter = "Txt Files (*.txt)| *.txt";
+
+            if (save.ShowDialog() != true)
+                return;
+
+            FileName = GetAbsolouteFilePath(save.FileName);
+
+            File.WriteAllText(FileName, data);
+        }
+
+        public void AutoSave(string data)
+        {
+            if (String.IsNullOrWhiteSpace(FileName))
+                return;
+
+            File.WriteAllText(FileName, data);
+        }
+        private string GetAbsolouteFilePath(string filePath)
+        {
+            return filePath.EndsWith(".txt") ? filePath : filePath + ".txt";
+        }
+    }
+}
---
+++ WordpadWPF/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WordpadWPF.Helpers;
+
+namespace WordpadWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileHelper _fileHelper;
+        public MainWindow()
+        {
+            _fileHelper = new FileHelper();
+
+            InitializeComponent();
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Text = _fileHelper.ReadDataFromFile();
+            TextBlockFilePath.Text = _fileHelper.FileName;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            _fileHelper.SaveDataToFile(TextBoxInput.Text);
+        }
+
+        private void TextBoxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!(sender is TextBox textBox))
+                return;
+
+            if (ToggleButtonAutoSave.IsChecked != true)
+                return;
+
+            _fileHelper.AutoSave(textBox.Text);
+        }
+
+        private void ButtonCut_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Cut();
+        }
+
+        private void ButtonCopy_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Copy();
+            TextBoxInput.Focus();
+        }
+
+        private void ButtonPaste_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Paste();
+        }
+
+        private void ButtonSelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Focus();
+            TextBoxInput.SelectAll();
+        }
+    }
+}
